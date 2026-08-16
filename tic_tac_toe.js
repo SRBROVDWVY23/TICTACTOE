04276da444981ec8a82e1f771b168eb62217a970
SRBROVDWVY23 <106043370+SRBROVDWVY23@users.noreply.github.com>
2026-08-15 23:51:13 -0500

    Add files via upload

---
+++ tic_tac_toe.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const getElement = selector => document.querySelector(selector);
+
+const setStatus = text => getElement("#status").textContent = text;
+
+const startGame = () => {
+    const game = new Game(getElement("canvas"));
+    setStatus(`${game.currentPlayer}'s turn`);
+    return game;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let game = startGame();
+
+    // event handler for canvas click event
+    getElement("canvas").addEventListener("click", evt => {
+        // get x and y coords of click using offsetX and offsetY
+        const x = evt.offsetX;
+        const y = evt.offsetY;
+       
+        game.takeTurn(x, y);
+
+        if (game.checkWinner()) {
+            setStatus(`${game.winner} wins!`);
+        } else if (game.isDraw) {
+            setStatus("Draw!");
+        } else {
+            setStatus(`${game.currentPlayer}'s turn`);
+        }
+    });
+
+    // event handler for New Game button click event
+    getElement("#new_game").addEventListener("click", () => {
+        game = startGame();
+    });
+});
